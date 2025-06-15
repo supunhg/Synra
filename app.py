971099f254964489b96fab6ccfac5a0fbf43cade
Supun Hewagamage <sanchithahewagamage@gmail.com>
2025-06-15 15:51:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -689,4 +689,5 @@ def get_session(session_id):
     return jsonify(list(messages))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
